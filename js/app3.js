5da635c441955d0cd741e9e129efba555b96c74a
Dario Vitorte <vitortedario@gmail.com>
2021-08-10 12:58:20 -0700

    yea

---
+++ js/app3.js
@@ -0,0 +1,65 @@
+let Color = prompt('Is My favorite color orange?')
+let fColor = Color.toLowerCase()
+if (fColor === 'yes' || fColor === 'y') {
+ // console.log('you are right i love orange!');
+  alert('You are right I love the color orange!');
+
+} else if (fColor === 'no' || fColor === 'n') {
+ // console.log('you are incorrect my favorite color is orange');
+  alert('Nope you are incorrect my favorite color is orange');
+
+} else {
+  alert('invalid response try again')
+}   
+let Sport = prompt('Is my favorite sport basketball?')
+let fSport = Sport.toLowerCase()
+if (fSport === 'yes' || fSport === 'y') {
+ // console.log('Nope you are incorrect my favorite sport is baseball');
+  alert('Nope you are incorrect my favorite sport is baseball');
+
+} else if (fSport === 'no' || fSport === 'n') {
+ //console.log('You are right! I actually love baseball!!');
+  alert('You are right! I actually love baseball!!');
+
+} else {
+  alert('invalid response try again')
+}
+let Food = prompt('Is my favorite food pizza?')  
+let fFood = Food.toLowerCase()
+if (fFood === 'yes' || fFood === 'y') {
+  //console.log('Nope you are incorrect my favorite food is sushi');
+  alert('Nope you are incorrect my favorite food is sushi');
+
+} else if (fFood === 'no' || fFood === 'n') {
+ //console.log('You are right! I actually love sushi');
+  alert('You are right! I actually love sushi');
+
+} else {
+  alert('invalid response try again')
+}
+let Place = prompt('Is my favorite place to travel Cuba?')
+let fPlace = Place.toLowerCase()
+if (fPlace === 'yes' || fPlace === 'y') {
+  //console.log('you are right i love visiting Cuba!');
+  alert('You are right I love visitng Cuba!');
+
+} else if (fPlace === 'no' || fPlace === 'n') {
+  //console.log('you are incorrect my favorite place to travel is Cuba');
+  alert('Nope you are incorrect my favorite place to travel is Cuba');
+
+} else {
+  alert('invalid response try again')
+}
+let PT = prompt('Is my favorite passtime gaming?')
+let fPT = PT.toLowerCase()
+if (fPT === 'yes' || fPT === 'y') {
+  //console.log('you are right i love gaming!');
+  alert('You are right I love gaming!');
+
+} else if (fPlace === 'no' || fPlace === 'n') {
+  //console.log('you are incorrect my favorite passtime is gaming');
+  alert('Nope you are incorrect my favorite passtime is gaming');
+
+} else {
+  alert('invalid response try again')
+}
