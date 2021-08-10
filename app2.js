15a7cd13c1bf98c451f404707de5cfb4c2bc1070
dar5o <87797682+dar5o@users.noreply.github.com>
2021-08-10 11:49:35 -0700

    Delete app2.js

--- app2.js
+++
@@ -1,4 +0,0 @@
-console.log("helloworld")
-let Ulocation = prompt('Where are you from?')
-let Uname = prompt('What is your name?')
-document.write('<p>Hi ' + Uname + ' from ' + Ulocation + ' </p>' )
